8ebd87d3677dfcb2731734892ac0e5e95037a08a
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2022-07-15 14:30:08 +0200

    feat: added verification that args are within int

--- src/check_args.c
+++ src/check_args.c
@@ -6,11 +6,40 @@
 /*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 17:12:41 by troberts          #+#    #+#             */
-/*   Updated: 2022/07/15 13:39:11 by troberts         ###   ########.fr       */
+/*   Updated: 2022/07/15 14:29:22 by troberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	check_bigger_then_int(char **array)
+{
+	size_t	i;
+	size_t	j;
+	size_t	lenght_int;
+	size_t	lenght;
+
+	i = 0;
+	j = 0;
+	lenght_int = 10;
+	while (array[i])
+	{
+		j = 0;
+		lenght = 0;
+		while (array[i][j])
+		{
+			if (ft_isdigit(array[i][j++]))
+				lenght++;
+		}
+		if (lenght > lenght_int || ft_atol(array[i]) > INT_MAX || \
+			ft_atol(array[i]) < INT_MIN)
+		{
+			ft_putendl_fd("Error", STDERR_FILENO);
+			exit (EXIT_FAILURE);
+		}
+		i++;
+	}
+}
 
 static void	check_duplicate(int *array, unsigned int size)
 {
@@ -32,7 +61,7 @@ static void	check_duplicate(int *array, unsigned int size)
 		if (counter != 1)
 		{
 			ft_putendl_fd("Error", STDERR_FILENO);
-			exit(EXIT_FAILURE);	
+			exit(EXIT_FAILURE);
 		}
 		i++;
 	}
@@ -75,5 +104,7 @@ t_bool	check_args(int ac, char **av, int **array_int, unsigned int *size)
 		(*size)++;
 	*array_int = convert_array_to_int(array_char);
 	check_duplicate(*array_int, *size);
+	check_bigger_then_int(array_char);
+	free_double_ptr_char(array_char);
 	return (true);
 }
